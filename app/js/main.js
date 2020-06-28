b8bdeac82d545e3985c40220c1da94aae441674c
shini322 <33940422+shini322@users.noreply.github.com>
2020-06-28 12:20:15 +0300

    322

--- app/js/main.js
+++ app/js/main.js
@@ -2,47 +2,6 @@
 //     $(this).toggleClass("active");
 //     $(".main-menu").toggleClass("active")
 //   });
-
-  let elem = document.getElementById('burger');
-  let elem1 = document.getElementById('menu-adaptive');
-  let elem2 = document.getElementById('wrapper');
-  elem.onclick = function(event){
-    elem.classList.toggle('active');    
-    elem1.classList.toggle('active');
-    elem2.classList.toggle('dark');
-  }
-  let elem3 = document.getElementById('filters__more-settings');
-  let elem4 = document.getElementById('filters__select-container--xl');
-  elem3.onclick = function(event){
-    elem4.classList.toggle('active');
-  }
-  let elem5 = document.getElementById('menu-adaptive__item--submenu');
-  let elem6 = document.getElementById('menu-adaptive__item--submenu-item');
-  elem5.onclick = function(event){
-    elem6.classList.toggle('active');
-  }
-
-  var galleryThumbs = new Swiper('.product-item__gallery-thumbs', {
-    spaceBetween: 10,
-    slidesPerView: 10,
-    breakpoints: {
-      768: {
-        slidesPerView: 3,
-      },
-      
-    },
-  });
-  var galleryTop = new Swiper('.product-item__gallery-top', {
-    spaceBetween: 10,
-    slidesPerView: 1,
-    
-    thumbs: {
-      swiper: galleryThumbs
-    }
-  });
-
-  
-  
 // document.addEventListener('DOMContentLoaded', createSelect, false);
 // function createSelect() {
 // 	var select = document.getElementsByTagName('select'),
@@ -127,3 +86,56 @@
 // 		}, false);
 // 	}
 // }
+
+  let elem = document.getElementById('burger');
+  let elem1 = document.getElementById('menu-adaptive');
+  let elem2 = document.getElementById('wrapper');
+  elem.onclick = function(event){
+    elem.classList.toggle('active');    
+    elem1.classList.toggle('active');
+  }
+  let elem3 = document.getElementById('filters__more-settings');
+  let elem4 = document.getElementById('filters__select-container--xl');
+  elem3.onclick = function(event){
+    elem4.classList.toggle('active-select');
+  }
+  let elem5 = document.getElementById('menu-adaptive__item--submenu');
+  let elem6 = document.getElementById('menu-adaptive__item--submenu-item');
+  elem5.onclick = function(event){
+    elem6.classList.toggle('active');
+  }
+
+  var galleryThumbs = new Swiper('.product-item__gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 10,
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+      },
+      
+    },
+  });
+  
+  var galleryTop = new Swiper('.product-item__gallery-top', {
+    spaceBetween: 10,
+    slidesPerView: 1,
+    
+    thumbs: {
+      swiper: galleryThumbs
+    }
+  });
+
+  {
+    
+}
+var starRatingStep = raterJs( {
+  starSize:25,
+  rating:4.5, 
+  step:0.5, 
+  element:document.querySelector("#rater"), 
+  rateCallback:function rateCallback(rating, done) {
+    this.setRating(rating); 
+    done(); 
+  }
+}); 
+  

e79d857b7a4557cb239351b414090e47d3ba5a6a
shini322 <33940422+shini322@users.noreply.github.com>
2020-06-29 12:14:29 +0300

    123

--- app/js/category.js
+++ app/js/category.js
@@ -29,6 +29,8 @@ let elem3 = document.getElementById('category__more-settings');
     }
   }); 
 
+  
+if (screen.width >= 1024) {
   //Получаем все "select" по селектору
 const selects = document.querySelectorAll('.custom-select')
 //переборка по полученным "select"
@@ -59,15 +61,15 @@ select.after(customSelect)
 
 //получаем список и значения "option" из "select", затем создаём кастомный "option" для кастоного "select"
 const createCustomDom = function(x, y){
-let selectItems = ''
-// for(option of options){
-// selectItems += `<button type="button" class="custom-select__item" data-value="${option.value}">${option.text}</button>`
-// }
-for(var i = 0; i < options.length; i++){
-selectItems += '<button type="button" class="custom-select__item" data-value="'+options[i].value+'">'+options[i].text+'</button>'
-}
-customSelectList.innerHTML = selectItems
-x(),y();
+  let selectItems = ''
+  // for(option of options){
+  // 	selectItems += `<button type="button" class="custom-select__item" data-value="${option.value}">${option.text}</button>`
+  // }
+  for(var i = 0; i < options.length; i++){
+    selectItems += '<button type="button" class="custom-select__item" data-value="'+options[i].value+'">'+options[i].text+'</button>'
+  }
+  customSelectList.innerHTML = selectItems
+  x(),y();
 }
 
 //открываем-закрываем выпадающий список по клику
@@ -76,31 +78,31 @@ const toggleClass = () => {customSelect.classList.toggle('custom-select--show')}
 //присваиваем текстовое первое значение "option" в кастомном "select"
 const currentTextValue = () => customSelectCurrent.textContent = customSelectList.children[0].textContent
 
-//получаем и задаем значения text/value
+//получаем и задаем значения text/value 
 const currentValue = () => {
-const items = customSelectList.children
-for(var el = 0; el < items.length; el++){
-let selectValue = items[el].getAttribute('data-value')
-let selectText = items[el].textContent
-items[el].addEventListener('click', () => {
-customSelect.classList.remove('custom-select--show')
-customSelectCurrent.textContent = selectText
-select.value = selectValue
-})
-}
+  const items = customSelectList.children
+  for(var el = 0; el < items.length; el++){
+    let selectValue = items[el].getAttribute('data-value')
+    let selectText = items[el].textContent
+    items[el].addEventListener('click', () => {
+      customSelect.classList.remove('custom-select--show')
+      customSelectCurrent.textContent = selectText
+      select.value = selectValue
+    })
+  }
 }
 
 const desctopFn = () => {
-customSelectCurrent.addEventListener('click', toggleClass)
+  customSelectCurrent.addEventListener('click', toggleClass)		
 }
 
 const mobileFn = () => {
-for(let j = 0; j < selects.length; j++){
-let mobileSelect = selects[j]
-mobileSelect.addEventListener('change', ()=> {
-mobileSelect.nextElementSibling.querySelector('.custom-select__current').textContent = mobileSelect.value
-})
-}
+  for(let j = 0; j < selects.length; j++){
+    let mobileSelect = selects[j]
+    mobileSelect.addEventListener('click', ()=> {
+      mobileSelect.nextElementSibling.querySelector('.custom-select__current').textContent = mobileSelect.value
+    })
+  }
 }
 
 createCustomDom(currentTextValue, currentValue)
@@ -108,26 +110,28 @@ createCustomDom(currentTextValue, currentValue)
 
 //закрываем выпадающий список по клику вне области кастомного селекта
 document.addEventListener('mouseup', (e) =>{
-if (!customSelect.contains(e.target)) customSelect.classList.remove('custom-select--show')
+  if (!customSelect.contains(e.target))	customSelect.classList.remove('custom-select--show')
 })
 
 detectmob(mobileFn, desctopFn)
 
-function detectmob(x,y) {
-if( navigator.userAgent.match(/Android/i)
-|| navigator.userAgent.match(/webOS/i)
-|| navigator.userAgent.match(/iPhone/i)
-|| navigator.userAgent.match(/iPad/i)
-|| navigator.userAgent.match(/iPod/i)
-|| navigator.userAgent.match(/BlackBerry/i)
-|| navigator.userAgent.match(/Windows Phone/i)
-){
-x();
+function detectmob(x,y) { 
+  if( navigator.userAgent.match(/Android/i)
+  || navigator.userAgent.match(/webOS/i)
+  || navigator.userAgent.match(/iPhone/i)
+  || navigator.userAgent.match(/iPad/i)
+  || navigator.userAgent.match(/iPod/i)
+  || navigator.userAgent.match(/BlackBerry/i)
+  || navigator.userAgent.match(/Windows Phone/i)
+  ){
+    x();
+  }
+  else {
+    y();
+   }
+ }
 }
-else {
-y();
-}
-}
-}
+
+} else {
   
-  
+}
